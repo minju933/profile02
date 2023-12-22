a1ef70d508daf91fe6a3583d5afa2ae6530b1c87
minju933 <153783786+minju933@users.noreply.github.com>
2023-12-22 18:01:11 +0900

    2023.12.22

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const header = document.querySelector('.header');
+const headerRect = header.getBoundingClientRect();
+const headerHeight = headerRect.height;
+
+
+document.addEventListener('scroll', function(){
+    console.log(window.scrollY);
+    if(window.scrollY > headerHeight) {
+        header.classList.add('header-dark');
+    } else {
+        header.classList.remove('header-dark');
+    }
+})
