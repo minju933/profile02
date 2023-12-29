88ecbbf4ef56e77a21c85be19c2dd97a155cc9d4
minju933 <153783786+minju933@users.noreply.github.com>
2023-12-29 16:01:58 +0900

    2023.12.29

--- js/main_05.js
+++ js/main_05.js
@@ -1,29 +1,19 @@
 //프로젝트 클릭하면 해당 프로젝트만 표현하기//
 
-const categories = document.querySelector('.category');
+const buttons = document.querySelectorAll('.category_button');
 const projects = document.querySelectorAll('.project');
-
-categories.addEventListener('click',(event) => {
-   // console.log(event);
-
-    const filter = event.target.dataset.category;
-    //console.log(filter);
-
-    projects.forEach((project) => {
-        console.log(project.dataset.type);
-        if(filter === 'all' || filter === project.dataset.type){
-            project.style.display = 'block';
-        } else {
-            project.style.display = 'none';
-        }
-        if (filter == null) {
-            return;
-        }
+buttons.forEach(function(button){
+    button.addEventListener('click', function(event){
+        const filter = event.target.dataset.category;
+        projects.forEach(function(project){
+            if (filter === 'all'|| filter === project.dataset.type) {
+                project.style.display = 'block';
+            } else {
+                project.style.display = 'none';
+            }
+        })
+        const selected = document.querySelector('.category_selected');
+       selected.classList.remove('category_selected');
+       event.target.classList.add('category_selected');
     })
-        const active_button = document.querySelector('.category_selected');
-        active_button.classList.remove('category_selected')
-
-        event.target.classList.add('category_selected');
-        console.log(event.target);
-
 })
